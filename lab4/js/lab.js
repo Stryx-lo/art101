a18bf90359187d7d9c7a81e7ced103fc832f2fa6
Stryx-lo <dragonendercat@gmail.com>
2024-10-17 19:06:05 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,11 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+/** Rock paper scissors
+*
+* An implementation of the game
+*
+* Ahmee Smith <ahmsmith@ucsc.edu>
+* since October 2024
+*/
 
 // Constants
 
@@ -19,3 +24,16 @@ function main() {
 
 // let's get this party started
 main();
+
+// Generate three options
+// Make those options rock, paper, and scissors
+// Make rock beat scissors, 
+// Make scissors beat paper
+// Make paper beat rock.
+//Make game loops
+// Computer chooses a random option
+	// when computer and player have chosen, play results
+// Track winner points
+// after a player wins a round, add one point to score
+// When one player reaches 3 points, play winner fanfare
+// Reset points
