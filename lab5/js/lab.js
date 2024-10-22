8ee96b893cb113a7f6ea37cd6eef71b4d94864a6
Stryx-lo <dragonendercat@gmail.com>
2024-10-21 18:50:49 -0700

    Lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables
+// Author: Ahmee Smith
+// Date: 21 October, 2024
 
-// Constants
 
-// Functions
+// Define Variables
+make = "Toyota";
+model = "RAV4";
+color = "pyrite";
+year = 2011;
+
+//calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
